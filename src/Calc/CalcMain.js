6568634f1436671568eecc9deedb9429337f28ce
Tyler Hueter <tylerhueter08@gmail.com>
2020-06-09 22:44:00 -0400

    spacer

--- src/Calc/CalcMain.js
+++ src/Calc/CalcMain.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Display from './Display';
 import Keypad from './Keypad';
+import Spacer from './Spacer';
 
 const CalcMain = () => {
     
     return (
         <div className='calc-body'>
             <Display />
+            <Spacer />
             <Keypad />
         </div>
     )
---
+++ src/Calc/Spacer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Spacer = () => {
+    return(
+        <span className='spacer'>
+            
+        </span>
+    )
+};
+
+export default Spacer;
