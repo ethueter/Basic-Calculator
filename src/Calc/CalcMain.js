9edbcc17c23688ad808282015c18a66a8be08e99
Tyler Hueter <tylerhueter08@gmail.com>
2020-06-16 23:02:29 -0400

    handleNum

--- src/Calc/CalcMain.js
+++ src/Calc/CalcMain.js
@@ -7,19 +7,24 @@ import {nums, opps} from './Equate';
 
 const CalcMain = () => {
     const [currNum, setCurrNum] = useState('');
-    const [display, setDisplay] = useState('1');
+    const [display, setDisplay] = useState('0');
     const [opp, setOpp] = useState('');
     const [equation, setEquation] = useState([]);
 
     const handleClick = (event) => {
-        let id = event.target.id
+        let id = event.target.id;
+        let val = event.target.innerText
     
         switch(true) {
             case nums.includes(id):
             console.log('number', id);
+            handleNum(val)
             break;
             case opps.includes(id):
             console.log('operand', id);
+            break;
+            case id === 'decimal':
+            console.log(id);
             break;
             default:
             console.log("didn't work")
@@ -27,13 +32,21 @@ const CalcMain = () => {
     };
 
     const handleClear = () => {
+        setCurrNum('');
         setDisplay('0');
         setEquation([]);
     };
 
-    const handleNum = () => {
-        
-    }
+    const handleNum = (num) => {
+        let newNum;
+        if (num === "0" && currNum.charAt(0) === "0") {
+            return num
+        } else {
+        newNum = currNum + num;
+        setCurrNum(newNum);
+        setDisplay(newNum);
+        };
+    };
     
     return (
         <div className='calc-body'>
