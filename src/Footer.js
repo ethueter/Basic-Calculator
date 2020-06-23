636f3827104ff899796b706811f03e6c6430f35c
Tyler Hueter <tylerhueter08@gmail.com>
2020-06-23 13:46:15 -0400

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Footer = () => {
+    
+    return (
+      <div className="footer">
+        <h3>
+          Simple Projects by{" "}
+          <a href="https://ethueter-resume.now.sh/">Tyler Hueter</a>
+        </h3>
+      </div>
+    );
+};
+
+export default Footer;
